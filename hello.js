be626d563f135bd7f4a9fee23260091a574fa2ab
patrick <tasniim_bari@yahoo.com>
2021-06-17 16:25:24 +0100

    added arrow to top function
    arrow to top will appear after scrolling down homepage, it will makeit easy for user to go back to the top

--- hello.js
+++ hello.js
@@ -8,3 +8,18 @@ navToggle.addEventListener('click', function(){
     }else{options.classList.add('show-links')};
     
 })
+
+// For arrow to top
+const scroll =document.querySelector(".scroll");
+
+window.addEventListener("scroll",function(){
+    console.log(`sop`);
+
+    const height =window.pageYOffset;
+
+    if(height>300){
+         scroll.classList.add('show-scroll');
+    }else{scroll.classList.remove('show-scroll')
+    }
+    console.log(height)
+})
